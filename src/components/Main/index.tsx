c647b43626a5a1baae9aa7d9b77025daa3be03ef
Vítor Emanuel Queiroz Ferreira <vitoremanuelqf@gmail.com>
2021-12-21 17:19:16 -0300

    add jest

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <main>
+      <h1>React Avançado</h1>
+    </main>
+  );
+};
+
+export default Main;
